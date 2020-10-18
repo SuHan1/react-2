4bd568b0f6985766eb301b0a5fe8a0db4167b69c
SuHan1 <m19832106712@163.com>
2020-10-18 17:02:01 +0800

    02

---
+++ build/precache-manifest.a63804d6df6aa19fcf7b62214399df0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448d121ea9caaec2c001db02650a9dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73648fa9c1ce9a1e0bc6",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a07a311cd40ca1acbac3",
+    "url": "/static/css/main.f2d531c5.chunk.css"
+  },
+  {
+    "revision": "73648fa9c1ce9a1e0bc6",
+    "url": "/static/js/2.bad3a0a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bad3a0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07a311cd40ca1acbac3",
+    "url": "/static/js/main.c747907a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
